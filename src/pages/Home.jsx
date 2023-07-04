45258a3475852041a8bf96fc9c5fece184067855
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-04 10:51:37 +0300

    added all the files on the Navigation bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import HomeComponent from '../components/HomeComponent'
+//import HomeComponent from '../components/HomeComponent'
 import '../components/Styles/Home.css'
+import myImage from '../assets/myImage.jpg'
 
 
 const Home=()=>{
   return (
-    <>
+    <div className='div'>
     <div>
     <Navbar/>
     </div>
-<div>
-    <HomeComponent/>
+    <div>
+      <img src={myImage} className="myImage" alt='MyImage'/>
+    </div>
+
+
 </div>
-
-
-
-</>
 
 
   )
